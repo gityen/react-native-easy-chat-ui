2e36eead09c415c315006ccfd24eb300af87b24a
gityen <38185567+gityen@users.noreply.github.com>
2019-10-04 21:40:54 +0800

    Update index.android.js

--- app/chat/emoji/index.android.js
+++ app/chat/emoji/index.android.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
-import { View, ScrollView, StyleSheet, ViewPagerAndroid, Platform, Dimensions, Animated, TouchableOpacity, Image } from 'react-native'
+import { View, ScrollView, StyleSheet, Platform, Dimensions, Animated, TouchableOpacity, Image } from 'react-native'
 import ViewPagerAndroidContainer from '../components/android-container'
+import ViewPager from "@react-native-community/viewpager";
 import Control from './control'
 import { EMOJIS_DATA, DEFAULT_EMOJI } from '../../source/emojis'
 const { width, height } = Dimensions.get('window')
@@ -35,7 +36,7 @@ export default class EmojiPanel extends PureComponent {
 
   render () {
     const { allPanelHeight, HeaderHeight } = this.props
-    const ContainerComponent = Platform.select({ ios: ScrollView, android: ViewPagerAndroid })
+    const ContainerComponent = Platform.select({ ios: ScrollView, android: ViewPager })
     this.total = 0
     return (
       <Animated.View style={[styles.container, {
